7da65f8db0e4b750755c73033ad7f7e5dc29677e
Noah Conchas <Conchasnoah8@gmail.com>
2025-03-18 10:37:37 -0700

    Implemented printTemperatures function

--- array_of_structs.cpp
+++ array_of_structs.cpp
@@ -7,7 +7,7 @@ using namespace std;
 // It should contain two integer fields: day and temperature.
 
 
-strunt TemperatureRecord{
+struct TemperatureRecord{
   int day;
   int temperature;
 };
@@ -18,31 +18,53 @@ strunt TemperatureRecord{
 const int MAX_DAYS = 31;
 
 // Function Prototypes
-void readTemperatures(int); // TODO: Fix the parameters
+
+ // TODO: Fix the parameters
+void readTemperatures(TemperatureRecord[], int&);
+void readTemperatures(int);
 void printTemperatures(const int);
 TemperatureRecord findMin(const int);
 TemperatureRecord findMax(const int);
 double findAverage(const double);
 
 int main() {
-    // TODO: Step 2 - Declare an array of TemperatureRecord structs (MAX_DAYS size)
-    TemperatureRecord[MAX_DAYS];
+    // TODO: Step 2 - Declare an array of TemperatureRecord structs (MAX_DAYS size
+	TemperatureRecord records[MAX_DAYS];
+	
+	 
+   
 
 
     
     int size = 0;  // Actual number of records read
 
-    // TODO: Step 3 - Call readTemperatures() to load data from file
 
-    // TODO: Step 4 - Print the temperatures
+    // TODO: Step 3 - Call readTemperatures() to load data from file
+	readTemperatures(records, size);
+	
+
+	// TODO: Step 4 - Print the temperatures
 
     // TODO: Step 5 - Compute and display min, max, and average temperature
 
     return 0;
 }
 
+
 // TODO: Step 6 - Implement readTemperatures()
 // Read from "temps.txt" and store data in the array
+void readTemperatures(TemperatureRecord records[], int& size) {
+	ifstream inputFile;
+	inputFile.open("temps.txt");
+	if (inputFile.fail()) {
+		cout << "Error: Could not open file" << endl;
+		return;
+	}
+	while (inputFile >> records[size].day >> records[size].temperature) {
+		size++;
+	}
+	inputFile.close();
+}
 
 // TODO: Step 7 - Implement printTemperatures()
 // Print all stored temperatures in a formatted table
